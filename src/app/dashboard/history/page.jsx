72d2127f35a5fec710ccf12523fe472f68697f45
TepChoulong <bizmavenhub@gmail.com>
2025-03-23 10:34:05 +0700

    add some table to history page

--- src/app/dashboard/history/page.jsx
+++ src/app/dashboard/history/page.jsx
@@ -1,14 +1,88 @@
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+import { Button } from "@/components/ui/button";
+
+const data = [
+  {
+    id: 1,
+    title: "This is the title of the article",
+    content: "This is the content of the article",
+    date: "2023-01-01",
+    wordsCount: 100,
+  },
+  {
+    id: 2,
+    title: "This is the title of the article",
+    content: "This is the content of the article",
+    date: "2023-01-01",
+    wordsCount: 100,
+  },
+  {
+    id: 3,
+    title: "This is the title of the article",
+    content: "This is the content of the article",
+    date: "2023-01-01",
+    wordsCount: 100,
+  },
+  {
+    id: 4,
+    title: "This is the title of the article",
+    content: "This is the content of the article",
+    date: "2023-01-01",
+    wordsCount: 100,
+  },
+];
+
 export default function Page() {
   return (
     <>
       <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-        <h1>History</h1>
-        <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-          <div className="aspect-video rounded-xl bg-muted/50" />
-          <div className="aspect-video rounded-xl bg-muted/50" />
-          <div className="aspect-video rounded-xl bg-muted/50" />
-        </div>
-        <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
+        <h1 className="text-2xl font-semibold">History</h1>
+        <Table className="w-full">
+          <TableHeader>
+            <TableRow>
+              <TableHead className="">ID</TableHead>
+              <TableHead className="">Title</TableHead>
+              <TableHead className="">Content</TableHead>
+              <TableHead className="">Date</TableHead>
+              <TableHead className="">Words Count</TableHead>
+              <TableHead className="text-center">Action</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {data.map((article) => {
+              return (
+                <TableRow key={article.id}>
+                  <TableCell className="font-medium">{article.id}</TableCell>
+                  <TableCell>{article.title}</TableCell>
+                  <TableCell>{article.content}</TableCell>
+                  <TableCell>{article.date}</TableCell>
+                  <TableCell>{article.wordsCount}</TableCell>
+                  <TableCell className="justify-center gap-2 flex">
+                    <Button
+                      variant="default"
+                      className="bg-indigo-600 hover:bg-indigo-700"
+                    >
+                      Download
+                    </Button>
+                    <Button className="bg-red-600 hover:bg-red-700">
+                      Delete
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
       </div>
     </>
   );
